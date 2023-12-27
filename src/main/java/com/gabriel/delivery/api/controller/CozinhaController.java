d6d7db4cf21a3edf299dd3a0121efd57f306c48b
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-12-27 17:22:01 -0300

    adicao cozinha controller

---
+++ src/main/java/com/gabriel/delivery/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.gabriel.delivery.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabriel.delivery.domain.model.Cozinha;
+import com.gabriel.delivery.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository repository;
+	
+	@GetMapping("/")
+	public List<Cozinha> listar() {
+		return repository.findAll();
+	}
+	
+}
