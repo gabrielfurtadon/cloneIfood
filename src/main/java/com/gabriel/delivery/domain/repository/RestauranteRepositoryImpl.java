167cd6b36bdc8e183fa158f54ba1d18d5e07b7c9
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-01-16 14:31:01 -0300

    alterando a query jpql para ficar dinamica

--- src/main/java/com/gabriel/delivery/domain/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/gabriel/delivery/domain/repository/RestauranteRepositoryImpl.java
@@ -1,14 +1,17 @@
 package com.gabriel.delivery.domain.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.gabriel.delivery.domain.model.Restaurante;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 @Repository
 public class RestauranteRepositoryImpl {
@@ -18,13 +21,31 @@ public class RestauranteRepositoryImpl {
 	
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteMin, BigDecimal taxaFreteMax) {
 		
-		var jpql = "from Restaurante where nome like :nome " + "and taxaFrete between :taxaFreteMin and :taxaFreteMax";
+		var jpql = new StringBuilder();
+		jpql.append("from Restaurante where 0 = 0");
+				
+		var parametros = new HashMap<String, Object>();
 		
-		return manager.createQuery(jpql, Restaurante.class)
-				.setParameter("nome", "%" + nome + "%")
-				.setParameter("taxaMin", taxaFreteMin) 
-				.setParameter("taxaMax", taxaFreteMax)
-				.getResultList();
+		if(StringUtils.hasLength(nome)) {
+			jpql.append("and nome like :nome ");
+			parametros.put("nome", "%" + nome + "%");
+		}
+		
+		if(taxaFreteMin != null) {
+			jpql.append("and taxaFrete >= :taxaFreteMin ");
+			parametros.put("taxaFreteMin", "taxaFreteMin");
+		}
+		
+		if(taxaFreteMax != null) {
+			jpql.append("and taxaFrete <= :taxaFreteMax ");
+			parametros.put("taxaFreteMax", "taxaFreteMax");
+		}
+		
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+		
+		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		
+		return query.getResultList();
 		
 	}
 	
