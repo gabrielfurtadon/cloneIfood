4df4a22ea63b126148059579cde4581c36ef5ad8
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-03-12 21:49:23 -0300

    att

---
+++ src/main/java/com/gabriel/delivery/domain/repository/CustomJpaRepository.java
@@ -0,0 +1,13 @@
+package com.gabriel.delivery.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean //Spring nao deve instanciar uma implementação para essa interface
+public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID>{
+
+	Optional<T> buscarPrimeiro();
+	
+}
