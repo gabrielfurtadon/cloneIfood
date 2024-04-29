3dd4db8c0fc6f19d30b7b150fcc55457af30c28a
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-04-29 12:10:06 -0300

    alteracao arquivo sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,13 @@ insert into restaurante (nome, taxa_frete, cozinha_id) values ('Bamboo', 10, 1);
 insert into estado(nome) values ('São Paulo');
 
 insert into cidade(nome, estado_id) values ('Itapetininga', 1);
+
+insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+insert into forma_pagamento (id, descricao) values (3, 'Pix');
+insert into forma_pagamento (id, descricao) values (4, 'Dinheiro');
+
+insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar as cozinhas existentes');
+insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar as cozinhas existentes');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2);
