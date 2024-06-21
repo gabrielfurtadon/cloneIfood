28ced0948e0f855ad93c4cc23b90e909b92a613d
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-06-20 21:06:12 -0300

    handling invalid parameter

--- src/main/java/com/gabriel/delivery/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/gabriel/delivery/api/exceptionHandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
 	MESSAGE_NOT_READABLE("/message-not-readable", "Message not readable"),
 	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
