5b35ec21441e01373f6bc1ee2bb369cb4d77ddad
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-06-25 21:31:16 -0300

    adding more constraints

--- src/main/java/com/gabriel/delivery/domain/model/Restaurante.java
+++ src/main/java/com/gabriel/delivery/domain/model/Restaurante.java
@@ -21,6 +21,9 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -35,10 +38,11 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
+	@DecimalMin("0")
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
