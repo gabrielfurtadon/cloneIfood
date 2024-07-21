51510b3917e0ae0dfa0c3cf27eed79e235543516
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-07-21 17:17:43 -0300

    more integration tests for cozinha

--- src/test/java/com/gabriel/delivery/CadastroCozinhaAPIIT.java
+++ src/test/java/com/gabriel/delivery/CadastroCozinhaAPIIT.java
@@ -1,6 +1,7 @@
 package com.gabriel.delivery;
 
-import org.flywaydb.core.Flyway;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,25 +9,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import com.gabriel.delivery.domain.model.Cozinha;
+import com.gabriel.delivery.domain.repository.CozinhaRepository;
+import com.gabriel.delivery.util.DatabaseCleaner;
+import com.gabriel.delivery.util.ResourceUtils;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaAPIIT {
 
+	private static final int COZINHA_ID_INEXISTENTE = 100;
+
+	private Cozinha cozinhaMexicana;
+	private int quantidadeCozinhasCadastradas;
+	private String jsonCorretoCozinhaChinesa;
+	
 	@Autowired
-	private Flyway flyway;
+	private DatabaseCleaner databaseCleaner;
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@BeforeEach
 	public void setup() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
-		flyway.migrate();
+		jsonCorretoCozinhaChinesa = ResourceUtils.getContentFromResource("/json/correct/cozinha-chinesa.json");
+		databaseCleaner.clearTables();
+		preparateData();
 	}
 	
 	@Test
@@ -40,27 +59,62 @@ public class CadastroCozinhaAPIIT {
 	}
 	
 	@Test
-	public void deveRetornar4CozinhasQuandoConsultarCozinhas() {
+	public void deveRetornarXCozinhasQuandoConsultarCozinhas() {
 		RestAssured.given()
 			.accept(ContentType.JSON)
 		.when()
 			.get()
 		.then()
-			.body("", Matchers.hasSize(4))
-			.body("nome", Matchers.hasItems("Tailandesa", "Indiana"));
+			.body("", Matchers.hasSize(quantidadeCozinhasCadastradas));
+			//.body("nome", Matchers.hasItems("Japonese", "Mexican"));
 	}
 	
 	@Test
 	public void deveRetornarStatus201QuandoCadastrarCozinha() {
 		RestAssured.given()
-		.body("{ \"nome\": \"Chinesa\" }")
-		.contentType(ContentType.JSON)
-		.accept(ContentType.JSON)
-	.when()
-		.post()
-	.then()
-		.statusCode(HttpStatus.CREATED.value());
+			.body(jsonCorretoCozinhaChinesa)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 	
+	@Test
+	public void deveRetornarRespostaEStatusCorretoQuandoConsultarCozinhaExistente() {
+		RestAssured.given()
+			.pathParam("cozinhaId", cozinhaMexicana.getId())
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cozinhaId}")
+		.then()
+			.statusCode(HttpStatus.OK.value())
+			.body("nome", equalTo(cozinhaMexicana.getNome()));
+	}
+	
+	@Test
+	public void deveRetornarRespostaEStatus404QuandoConsultarCozinhaInexistente() {
+		RestAssured.given()
+			.pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cozinhaId}")
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
+	}
+	
+	
+	private void preparateData() {
+		Cozinha c1 = new Cozinha();
+		c1.setNome("Japonese");
+		cozinhaRepository.save(c1);
+		
+		cozinhaMexicana = new Cozinha();
+		cozinhaMexicana.setNome("Mexican");
+		cozinhaRepository.save(cozinhaMexicana);
+		
+		quantidadeCozinhasCadastradas = (int) cozinhaRepository.count();
+	}
 	
 }
